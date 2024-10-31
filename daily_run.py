6797a920a1003d72c3a6cf2907a9d7b8326af002
bob7452 <ponder5247@gmail.com>
2024-10-31 19:20:19 +0800

    [v1.5] avoid run in holidays

--- daily_run.py
+++ daily_run.py
@@ -2,6 +2,8 @@ from data_analysis import Ath_model
 from datetime import datetime
 from shutil import rmtree
 import os
+from pandas_market_calendars import get_calendar
+import sys
 
 def restore():
     rmtree(path="rs_report")
@@ -14,14 +16,30 @@ def restore():
     os.mkdir("classic")
     os.mkdir("all")
     
+def isholidays(start_date):
+
+    nyse = get_calendar('NYSE')
+    
+    is_holidays = nyse.valid_days(start_date=start_date, end_date=start_date)
+
+    if is_holidays.empty:
+        print('plz enjoy ur holidays')
+        return 1
+    else:
+        print('Fighting')
+        return 0
+
 if __name__ == "__main__":
+
+    RANGE = 10
+    yy,mm,dd  = map(int,datetime.today().strftime("%Y-%m-%d").split('-'))
+    START = datetime(year=yy, month=mm, day=dd-1,hour=8)
+    END = datetime(year=yy, month=mm, day=dd-1,hour=8)
+
+    if isholidays(start_date=START):
+        sys.exit(1)
 
     restore()
 
-    yy,mm,dd  = map(int,datetime.today().strftime("%Y-%m-%d").split('-'))
-
-    RANGE = 10
-    START = datetime(year=yy, month=mm, day=dd-1,hour=8)
-    END = datetime(year=yy, month=mm, day=dd-1,hour=8)
     ath_model = Ath_model(start_date=START,end_date=END,gap_to_high_range=RANGE,reuse_data=False)
     ath_model.run()
